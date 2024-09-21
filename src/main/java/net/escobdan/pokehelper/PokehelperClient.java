3a5f3d0e2d3fa4f8e4d4c788c1a5c854389fe984
escobdan <escobdan1@gmail.com>
2024-09-20 19:55:54 -0600

    first commit

---
+++ src/main/java/net/escobdan/pokehelper/PokehelperClient.java
@@ -0,0 +1,11 @@
+package net.escobdan.pokehelper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PokehelperClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+        
+    }
+    
+}
---
+++ src/main/java/net/escobdan/pokehelper/PokehelperDataGenerator.java
@@ -0,0 +1,11 @@
+package net.escobdan.pokehelper;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PokehelperDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
